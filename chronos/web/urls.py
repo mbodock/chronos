698f1c020b3755a0bac3df2598d3cb3cc9ca8294
Hugo Leonardo Leão Mota <hugo.txt@gmail.com>
2015-06-16 22:44:57 -0300

    Add support to static files in web app

--- chronos/web/urls.py
+++ chronos/web/urls.py
@@ -1,6 +1,11 @@
+from flask import send_from_directory
 from .app import app
 from .views.register_user_view import RegisterUserView
 
+
+@app.route('/static/<path:path>')
+def get_static(path):
+    return send_from_directory('static', path)
 
 @app.route('/')
 def get_register_user():
