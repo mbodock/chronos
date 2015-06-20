4e84159a5fba2f8cc6ae0253dcbd3d8a9e6d13ba
Hugo Leonardo Leão Mota <hugo.txt@gmail.com>
2015-06-20 14:34:23 -0300

    Add UserTest base to simplify tests that needs users

--- chronos/tests/feature_tests/clock_feature_test.py
+++ chronos/tests/feature_tests/clock_feature_test.py
@@ -1,43 +1,34 @@
-from chronos.tests.test import Test
-
-from chronos.features.register_user_feature import RegisterUserFeature
+from .user_test import UserTest
 from chronos.features.clock_feature import ClockFeature
-
 from chronos.data.database import database
-from chronos.data.entities import User, Clock
+from chronos.data.entities import Clock
 
 
-class ClockFeatureTest(Test):
+class ClockFeatureTest(UserTest):
 
     def setUp(self):
         self.clock = ClockFeature()
-        self.setup_user()
 
     def tearDown(self):
         database.query(Clock).delete()
-        database.query(User).delete()
-
-    def setup_user(self):
-        self.register = RegisterUserFeature()
-        self.user = self.register.register_user('foo@bar.com', '123456')
 
     def test_start_clock(self):
-        self.clock.start_clock(self.user)
-        clocks = self.clock.get_clocks_from_user(self.user)
+        self.clock.start_clock(self.employee)
+        clocks = self.clock.get_clocks_from_user(self.employee)
         self.assertEqual(clocks.count(), 1)
         self.assertIsNone(clocks.one().stop)
 
     def test_stop_clock(self):
-        self.clock.start_clock(self.user)
-        self.clock.stop_clock(self.user)
-        clock = self.clock.get_clocks_from_user(self.user).one()
+        self.clock.start_clock(self.employee)
+        self.clock.stop_clock(self.employee)
+        clock = self.clock.get_clocks_from_user(self.employee).one()
         self.assertIsNotNone(clock.stop)
 
     def test_cannot_stop_clock_if_not_started(self):
         with self.assertRaises(ClockFeature.UnstartedClock):
-            self.clock.stop_clock(self.user)
+            self.clock.stop_clock(self.employee)
 
     def test_cannot_start_clock_if_already_started(self):
-        self.clock.start_clock(self.user)
+        self.clock.start_clock(self.employee)
         with self.assertRaises(ClockFeature.ClockAlreadyStarted):
-            self.clock.start_clock(self.user)
+            self.clock.start_clock(self.employee)
---
+++ chronos/tests/feature_tests/user_test.py
@@ -0,0 +1,19 @@
+from chronos.tests.test import Test
+from chronos.features.register_user_feature import RegisterUserFeature
+from chronos.data.database import database
+from chronos.data.entities import User
+
+
+class UserTest(Test):
+
+    @classmethod
+    def setUpClass(self):
+        super(UserTest, self).setUpClass()
+        feature = RegisterUserFeature()
+        self.admin = feature.register_user('admin@chronos.com', '123456')
+        self.employee = feature.register_user('employee@chronos.com', '123456')
+
+    @classmethod
+    def tearDownClass(self):
+        super(UserTest, self).tearDownClass()
+        database.query(User).delete()
